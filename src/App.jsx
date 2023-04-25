901400ecd65b8923747a33db820904bb0e01ef19
neerajkumar790652 <92433406+neerajkumar790652@users.noreply.github.com>
2023-04-25 10:00:57 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <div className="container">
+      <div className='card'>
+      <div>
+        <img src='https://media.istockphoto.com/id/1270067126/photo/smiling-indian-man-looking-at-camera.jpg?s=612x612&w=0&k=20&c=ovIQ5GPurLd3mOUj82jB9v-bjGZ8updgy1ACaHMeEC0=' style={{width:"300px",height:'150px'}}/>
+      </div>
+      <div className='info'>
+        <h1>Dima Blover</h1>
+        <p>UI/UX Designer</p>
+        <div className='follower'>
+          <div>
+            <span>26</span>
+            <br/>
+            <span>Shoot</span>
+          </div>
+          <div>
+          <span>94</span>
+          <br/>
+            <span>Followers</span>
+          </div>
+          <div>
+          <span>156</span>
+          <br/>
+            <span>Following</span>
+          </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+    
+    
+  )
+}
+
+export default App
